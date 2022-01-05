2a93acff4837837b8689fe2e23aeb006ce0fd7a3
acer90 <acer151090@aol.com>
2022-01-05 02:51:54 +0100

    Update BlueIris Module
    Update BlueIris Module auf Version 2.0

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
-[![Version](https://img.shields.io/badge/Symcon-PHPModul-red.svg)](https://www.symcon.de/service/dokumentation/entwicklerbereich/sdk-tools/sdk-php/) 
-[![Version](https://img.shields.io/badge/Symcon%20Version-4.3%20%3E-green.svg)](https://www.symcon.de/forum/threads/30857-IP-Symcon-4-3-%28Stable%29-Changelog)
+[![Version](https://img.shields.io/badge/Symcon-PHPModul-red.svg)](https://www.symcon.de/service/dokumentation/entwicklerbereich/sdk-tools/sdk-php/)
+[![Version](https://img.shields.io/badge/Modul%20Version-0.9-blue.svg)]()
+[![Version](https://img.shields.io/badge/Symcon%20Version-5.3%20%3E-green.svg)](https://www.symcon.de/forum/threads/30857-IP-Symcon-5-3-%28Stable%29-Changelog)
 
-# SymconModule
-Diese Library wurde von Acer90 erstellt, sie enthält mehrere Module für IP-Symcon ab Version 4.3 
+# JSlive
+Die Bibiliotek dient zum verteilen der JSLive-Module im Modulstore
+Alle meine Module unter: https://github.com/Acer90/SymconModule
 
 ## Dokumentation
 
@@ -10,40 +12,39 @@ Diese Library wurde von Acer90 erstellt, sie enthält mehrere Module für IP-Sym
 
 1. [Funktionsumfang](#1-funktionsumfang)  
 2. [Voraussetzungen](#2-voraussetzungen)  
-3. [Software-Installation](#3-software-installation) 
-4. [Einrichten der Instanzen in IP-Symcon](#4-einrichten-der-instanzen-in-ip-symcon)
-5. [Anhang](#5-anhang)  
-    1. [GUID der Module](#1-guid-der-module)
-6. [Lizenz](#6-lizenz)
+3. [Lizenz](#6-lizenz)
 
 ## 1. Funktionsumfang
 
-- __SymconBlueIris__ ([Dokumentation](SymconBlueIris))  
-	Liest Informationen aus der BlueIris Kamera Software aus.  
+- __SymconJSLive__ ([Dokumentation](SymconJSLive))  
+	Splitter für alle JSLive Module
+	
+- __SymconJSLiveChart__ ([Dokumentation](SymconJSLiveChart))  
+	Modul zum erstellen von Balken- und Liniendiagrammen
+	
+- __SymconJSLiveChartRadar__ ([Dokumentation](SymconJSLiveChartRadar))  
+	Modul zum erstellen von Radardiagrammen
+	
+- __SymconJSLiveColorPicker__ ([Dokumentation](SymconJSLiveColorPicker))  
+	Modul zum erstellen von Colorpicker
+	
+- __SymconJSLiveCustom__ ([Dokumentation](SymconJSLiveCustom))  
+	Modul zum erstellen von Eigner Javasriptausgaben auf basis von JSLive
+	
+- __SymconJSLiveDoughnutPie__ ([Dokumentation](SymconJSLiveDoughnutPie))  
+	Modul zum erstellen von Donat- und Tortendiagrammen
+	
+- __SymconJSLiveGauge__ ([Dokumentation](SymconJSLiveGauge))  
+	Modul zum erstellen von animierte Messinstrumente(Gauges)
+	
+- __SymconJSLiveProgressbar__ ([Dokumentation](SymconJSLiveProgressbar))  
+	Modul zum erstellen von anpassbaren Progressbars
 
 ## 2. Voraussetzungen
 
- - IPS 4.3 oder höher  
+ - IPS 5.3 oder höher  
 
-## 3. Software-Installation
-
-**ab IPS 4.3:**  
-    `https://github.com/Acer90/SymconModule`
-
-## 4. Einrichten der Instanzen in IP-Symcon
-
-Details sind in der Dokumentation der jeweiligen Module beschrieben.  
-
-## 5. Anhang
-
-###  1. GUID der Module
-
-| Modul                  | Typ      | Prefix       | GUID                                   |
-| :--------------------: | :------: | :----------: | :------------------------------------: |
-| SymconBlueIris         | Device   | BlueIris     | {7E62F9B0-5474-426F-B91B-E25F4B25A824} |
-| SymconBlueIrisCam      | Device   | BlueIrisCam  | {5308D185-A3D2-42D0-B6CE-E9D3080CE184} |
-
-## 6. Lizenz
+## 3. Lizenz
 
   IPS-Modul:  
   [GNU GENERAL PUBLIC LICENSE](http://www.gnu.org/licenses/)  
--- SymconBlueIrisCam/module.php
+++ SymconBlueIrisCam/module.php
@@ -1,141 +1,281 @@
-<?
+<?php
     // Klassendefinition
-    class BlueIrisCam extends IPSModule {
-        public function __construct($InstanceID) {
+    class SymconBlueIrisCam extends IPSModule {
+        public function __construct(int $InstanceID) {
             parent::__construct($InstanceID);
         }
 
         public function Create() {
             parent::Create();
 
+            $this->ConnectParent("{E138AFDC-D1E0-B462-A5E5-AF24F57D4686}");
+
             // Modul-Eigenschaftserstellung
-            $this->RegisterPropertyString("ShortName", "cam1");
-            $this->SetStatus(102);
+            $this->RegisterPropertyString("ShortName", "");
+            $this->RegisterPropertyBoolean("PTZ", false);
+            $this->RegisterPropertyBoolean("showFPS", false);
         }
 
-        public function ReceiveData($JSONString) {
+        public function ApplyChanges()
+        {
+            //Never delete this line!
+            parent::ApplyChanges();
+            $this->SetReceiveDataFilter('.*sname\\\":[ \\\"]*(' . $this->ReadPropertyString("ShortName") . '|0)[\\\”]*.*');
 
-            $data = json_decode($JSONString);
-            IPS_LogMessage("ReceiveData", utf8_decode($data->CreateVar));
+            //create variables
+            $this->RegisterVariableBoolean("isAlerting",  $this->Translate("Alert"), "~Switch", 1);
+            $this->RegisterVariableBoolean("isMotion",  $this->Translate("Motion"), "~Switch", 2);
+            $this->RegisterVariableBoolean("isTriggered",  $this->Translate("Triggered"), "~Switch", 3);
+            $this->RegisterVariableBoolean("isRecording",  $this->Translate("Recording"), "~Switch", 4);
 
-            if($createVar){
-                if(@IPS_GetVariableIDByName("isOnline", $key) === False){
-                    $VarID = IPS_CreateVariable(0);
-                    IPS_SetName($VarID, "isOnline"); // Variable benennen
-                    IPS_SetParent($VarID, $InsID);
-                    IPS_SetVariableCustomProfile($VarID, "~Switch");
+            $this->RegisterVariableBoolean("isOnline",  $this->Translate("Online"), "~Switch", 90);
+            $this->RegisterVariableBoolean("isEnabled",  $this->Translate("Enabled"), "~Switch", 90);
+            $this->RegisterVariableBoolean("isPaused",  $this->Translate("Pause"), "~Switch", 91);
+            $this->RegisterVariableBoolean("isNoSignal",  $this->Translate("NoSignal"), "~Switch", 92);
+
+            $this->EnableAction("isTriggered");
+            $this->EnableAction("isEnabled");
+            $this->EnableAction("isPaused");
+
+            if($this->ReadPropertyBoolean("PTZ")){
+                if (!IPS_VariableProfileExists("BlueIris.Preset")){
+                    IPS_CreateVariableProfile("BlueIris.Preset", 1);
+                    IPS_SetVariableProfileAssociation("BlueIris.Preset", 101, $this->Translate("PTZ-Preset")." 1", "", 0x00ff00);
+                    IPS_SetVariableProfileAssociation("BlueIris.Preset", 102, $this->Translate("PTZ-Preset")." 2", "", 0x00ff00);
+                    IPS_SetVariableProfileAssociation("BlueIris.Preset", 103, $this->Translate("PTZ-Preset")." 3", "", 0x00ff00);
+                    IPS_SetVariableProfileAssociation("BlueIris.Preset", 104, $this->Translate("PTZ-Preset")." 4", "", 0x00ff00);
+                    IPS_SetVariableProfileAssociation("BlueIris.Preset", 105, $this->Translate("PTZ-Preset")." 5", "", 0x00ff00);
+                    IPS_SetVariableProfileAssociation("BlueIris.Preset", 106, $this->Translate("PTZ-Preset")." 6", "", 0x00ff00);
+                    IPS_SetVariableProfileAssociation("BlueIris.Preset", 107, $this->Translate("PTZ-Preset")." 7", "", 0x00ff00);
+                    IPS_SetVariableProfileAssociation("BlueIris.Preset", 108, $this->Translate("PTZ-Preset")." 8", "", 0x00ff00);
+                    IPS_SetVariableProfileAssociation("BlueIris.Preset", 109, $this->Translate("PTZ-Preset")." 9", "", 0x00ff00);
+                    IPS_SetVariableProfileAssociation("BlueIris.Preset", 110, $this->Translate("PTZ-Preset")." 10", "", 0x00ff00);
+                    IPS_SetVariableProfileAssociation("BlueIris.Preset", 111, $this->Translate("PTZ-Preset")." 11", "", 0x00ff00);
+                    IPS_SetVariableProfileAssociation("BlueIris.Preset", 112, $this->Translate("PTZ-Preset")." 12", "", 0x00ff00);
+                    IPS_SetVariableProfileAssociation("BlueIris.Preset", 113, $this->Translate("PTZ-Preset")." 13", "", 0x00ff00);
+                    IPS_SetVariableProfileAssociation("BlueIris.Preset", 114, $this->Translate("PTZ-Preset")." 14", "", 0x00ff00);
+                    IPS_SetVariableProfileAssociation("BlueIris.Preset", 115, $this->Translate("PTZ-Preset")." 15", "", 0x00ff00);
+                    IPS_SetVariableProfileAssociation("BlueIris.Preset", 116, $this->Translate("PTZ-Preset")." 16", "", 0x00ff00);
+                    IPS_SetVariableProfileAssociation("BlueIris.Preset", 117, $this->Translate("PTZ-Preset")." 17", "", 0x00ff00);
+                    IPS_SetVariableProfileAssociation("BlueIris.Preset", 118, $this->Translate("PTZ-Preset")." 18", "", 0x00ff00);
+                    IPS_SetVariableProfileAssociation("BlueIris.Preset", 119, $this->Translate("PTZ-Preset")." 19", "", 0x00ff00);
+                    IPS_SetVariableProfileAssociation("BlueIris.Preset", 120, $this->Translate("PTZ-Preset")." 20", "", 0x00ff00);
+                }
+                if (!IPS_VariableProfileExists("BlueIris.Move")){
+                    IPS_CreateVariableProfile("BlueIris.Move", 1);
+                    IPS_SetVariableProfileValues ("BlueIris.Move", -1, 1, 1);
                 }
 
-                if(@IPS_GetVariableIDByName("isRecording", $key) === False){
-                    $VarID = IPS_CreateVariable(0);
-                    IPS_SetName($VarID, "isRecording"); // Variable benennen
-                    IPS_SetParent($VarID, $InsID);
-                    IPS_SetVariableCustomProfile($VarID, "~Switch");
-                }
+                $this->RegisterVariableInteger("PTZPreset",  $this->Translate("PTZ-Preset"), "BlueIris.Preset", 10);
+                $this->RegisterVariableInteger("PTZMoveLeftRight",  $this->Translate("PTZ-Move Left/Right"), "BlueIris.Move", 11);
+                $this->RegisterVariableInteger("PTZMoveUPDown",  $this->Translate("PTZ-Move UP/Down"), "BlueIris.Move", 12);
+                $this->RegisterVariableInteger("PTZMoveZoom",  $this->Translate("PTZ-Move Zoom"), "BlueIris.Move", 13);
 
-                if(@IPS_GetVariableIDByName("Stream", $key) === False){
-                    $ImageFile = 'http://'.$IPAddress.":".$Port."/mjpg/". $val["optionValue"]. "/video.mjpg";     // Image-Datei
-                    $MediaID = IPS_CreateMedia(3);                  // Image im MedienPool anlegen
-                    IPS_SetMediaFile($MediaID, $ImageFile, true);   // Image im MedienPool mit Image-Datei verbinden
-                    IPS_SetName($MediaID, "Stream"); // Medienobjekt benennen
-                    IPS_SetParent($MediaID, $InsID);
-                }
+                $this->EnableAction("PTZPreset");
+                $this->EnableAction("PTZMoveLeftRight");
+                $this->EnableAction("PTZMoveUPDown");
+                $this->EnableAction("PTZMoveZoom");
 
-                if(@IPS_GetVariableIDByName("FPS", $key) === False){
-                    $VarID = IPS_CreateVariable(2);
-                    IPS_SetName($VarID, "FPS"); // Variable benennen
-                    IPS_SetParent($VarID, $InsID);
-                }
+                if($this->GetValue("PTZPreset") < 101 || $this->GetValue("PTZPreset") > 120) $this->SetValue("PTZPreset", 101);
             }
 
-            $VarID = @IPS_GetVariableIDByName("isOnline", $key);
-            if($VarID !== False){
-                if(!empty($val["isOnline"])) SetValueBoolean($VarID, True); else SetValueBoolean($VarID, False);
-            }
-
-            $VarID = @IPS_GetVariableIDByName("isRecording", $key);
-            if($VarID !== False){
-                if(!empty($val["isRecording"])) SetValueBoolean($VarID, True); else SetValueBoolean($VarID, False);
-            }
-
-            $VarID = @IPS_GetVariableIDByName("FPS", $key);
-            if($VarID !== False){
-                if(!empty($val["FPS"])) SetValue($VarID,$val["FPS"]); else SetValue($VarID, 0);
+            if($this->ReadPropertyBoolean("showFPS")){
+                $this->RegisterVariableInteger("FPS",  $this->Translate("FPS"), "", 99);
             }
         }
 
-        public function AlertList(int $startdate = null, bool $reset = null){
-            if(is_null($startdate)) $startdate = 0;
-            if(is_null($reset)) $reset = false;
+        public function RequestAction($Ident, $Value) {
+            switch($Ident) {
+                case "PTZPreset":
+                    if($Value >= 101 && $Value <= 120){
+                        $this->PTZ($Value,0);
+                    }
+                    $this->SetValue($Ident, $Value);
+                    break;
+                case "PTZMoveLeftRight":
+                    if($Value < 0){
+                        $this->PTZ(0,0);
+                    }elseif($Value > 0){
+                        $this->PTZ(1,0);
+                    }
+                    $this->SetValue($Ident, 0);
+                    break;
+                case "PTZMoveUPDown":
+                    if($Value > 0){
+                        $this->PTZ(2,0);
+                    }elseif($Value < 0){
+                        $this->PTZ(3,0);
+                    }
+                    $this->SetValue($Ident, 0);
+                    break;
+                case "PTZMoveZoom":
+                    if($Value > 0){
+                        $this->PTZ(5,0);
+                    }elseif($Value < 0){
+                        $this->PTZ(6,0);
+                    }
+                    $this->SetValue($Ident, 0);
+                    break;
+                case "isEnabled":
+                case "isPaused":
+                    $this->SetValue($Ident, $Value);
+                    $this->UpdateCamConfig();
+                    break;
+                case "isTriggered":
+                    $this->SetValue($Ident, $Value);
+                    if($Value){
+                        $this->SendDebug(__FUNCTION__, $this->Trigger(), 0);
+                    }
+                    break;
+                default:
+                    throw new Exception("Invalid Ident");
+            }
+        }
 
-            $id = $this->InstanceID;
-            $pid = IPS_GetParent($id);
-            $camera = $this->ReadPropertyString("ShortName");
+        public function ReceiveData($JSONString)
+        {
+            $rData = json_decode($JSONString, true);
+            $buffer = json_decode($rData["Buffer"], true);
 
-            $sid = BlueIris_Login($pid);
-            if($sid != "ERROR")
-                return BlueIris_AlertList($pid,$sid, $camera,$startdate, $reset);
-            else
-                return  "ERROR";
+            switch($buffer['cmd']) {
+                case "CamList":
+                    return $this->UpdateConfig($buffer['payload']);
+                default:
+                    $this->SendDebug(__FUNCTION__, "ACTION " . $buffer['cmd'] . " FOR THIS MODULE NOT DEFINED!", 0);
+                    break;
+            }
+        }
+
+        private function UpdateConfig($config){
+            //$this->SendDebug(__FUNCTION__, json_encode($config), 0);
+            $this->SetValue("isEnabled", $config["isEnabled"]);
+            $this->SetValue("isAlerting", $config["isAlerting"]);
+            $this->SetValue("isMotion", $config["isMotion"]);
+            $this->SetValue("isTriggered", $config["isTriggered"]);
+            $this->SetValue("isRecording", $config["isRecording"]);
+            $this->SetValue("isOnline", $config["isOnline"]);
+            $this->SetValue("isPaused", $config["isPaused"]);
+            $this->SetValue("isNoSignal", $config["isNoSignal"]);
+
+            if($this->ReadPropertyBoolean("showFPS")){
+                $this->SetValue("FPS", $config["FPS"]);
+            }
+        }
+
+        private function UpdateCamConfig(){
+            $enable = $this->GetValue("isEnabled");
+            if($this->GetValue("isPaused")){
+                $pause = -1; //-1 pause| 0 stop pause
+            }else{
+                $pause = 0; //-1 pause| 0 stop pause
+            }
+
+            $result = $this->CamConfig(null, $enable, $pause);
+            $this->SendDebug(__FUNCTION__, $result, 0);
+
+            return $result;
         }
 
         public function CamConfig(bool $reset = null, bool $enable = null, int $pause = null, bool $motion = null, bool $schedule = null, bool $ptzcycle = null, bool $ptzevents = null, int $alerts = null, int $record = null){
-            $id = $this->InstanceID;
-            $pid = IPS_GetParent($id);
             $camera = $this->ReadPropertyString("ShortName");
 
-            $sid = BlueIris_Login($pid);
-            if($sid != "ERROR")
-                return BlueIris_CamConfig($pid, $sid, $camera, $reset, $enable, $pause, $motion, $schedule, $ptzcycle, $ptzevents, $alerts, $record);
-            else
-                return  "ERROR";
+            $data = array();
+            $data["camera"] = $camera;
+            if(!is_null($reset)) $data["reset"] = $reset;
+            if(!is_null($enable)) $data["enable"] = $enable;
+            if(!is_null($pause)) $data["pause"] = $pause;
+            if(!is_null($motion)) $data["motion"] = $motion;
+            if(!is_null($schedule)) $data["schedule"] = $schedule;
+            if(!is_null($ptzcycle)) $data["ptzcycle"] = $ptzcycle;
+            if(!is_null($ptzevents)) $data["ptzevents"] = $ptzevents;
+            if(!is_null($alerts)) $data["alerts"] = $alerts;
+            if(!is_null($record)) $data["record"] = $record;
+
+            $sendData = array("cmd" => "CamConfig", "cam" => $camera, "data" => $data);
+            return $this->SendDataToParent(json_encode([
+                'DataID' => "{95E9AD15-F5B0-01C6-4FF2-9093B9BD4E36}",
+                'Buffer' => utf8_encode(json_encode($sendData))
+            ]));
         }
 
+        public function AlertList(int $startdate = null, bool $reset = null){
+            $camera = $this->ReadPropertyString("ShortName");
+
+            $data = array();
+            $data["camera"] = $camera;
+            if(is_null($camera)) $camera = "index";
+            if(is_null($startdate)) $startdate = 0;
+            if(is_null($reset)) $reset = false;
+
+            $sendData = array("cmd" => "AlertList", "data" => $data);
+            return $this->SendDataToParent(json_encode([
+                'DataID' => "{95E9AD15-F5B0-01C6-4FF2-9093B9BD4E36}",
+                'Buffer' => utf8_encode(json_encode($sendData))
+            ]));
+        }
         public function ClipList(int $startdate = null, int $enddate = null, bool $tiles = null){
+            $camera = $this->ReadPropertyString("ShortName");
+
+            $data = array();
+            $data["camera"] = $camera;
             if(is_null($startdate)) $startdate = 0;
             if(is_null($enddate)) $enddate = time();
             if(is_null($tiles)) $tiles = false;
 
-            $id = $this->InstanceID;
-            $pid = IPS_GetParent($id);
+            $sendData = array("cmd" => "ClipList","data" => $data);
+            return $this->SendDataToParent(json_encode([
+                'DataID' => "{95E9AD15-F5B0-01C6-4FF2-9093B9BD4E36}",
+                'Buffer' => utf8_encode(json_encode($sendData))
+            ]));
+        }
+        public function PTZ(int $button = 4, int $updown = null){
             $camera = $this->ReadPropertyString("ShortName");
 
-            $sid = BlueIris_Login($pid);
-            if($sid != "ERROR")
-                return BlueIris_ClipList($pid, $sid, $camera, $startdate, $enddate, $tiles);
-            else
-                return  "ERROR";
-        }
-
-        public function PTZ(int $button = null, int $updown = null){
-            if(is_null($button)){
-                $this->SetStatus(203);
-                return "ERROR";
-            } 
+            $data = array();
+            $data["camera"] = $camera;
+            $data["button"] = $button;
             if(is_null($updown)) $updown = 0;
 
-            $id = $this->InstanceID;
-            $pid = IPS_GetParent($id);
+            $sendData = array("cmd" => "PTZ", "data" => $data);
+            return $this->SendDataToParent(json_encode([
+                'DataID' => "{95E9AD15-F5B0-01C6-4FF2-9093B9BD4E36}",
+                'Buffer' => utf8_encode(json_encode($sendData))
+            ]));
+        }
+        public function Trigger(){
             $camera = $this->ReadPropertyString("ShortName");
 
-            $sid = BlueIris_Login($pid);
-            if($sid != "ERROR")
-                return BlueIris_PTZ($pid, $sid, $camera, $button, $updown);
-            else
-                return "ERROR";
+            $data = array();
+            $data["camera"] = $camera;
+
+            $sendData = array("cmd" => "Trigger", "data" => $data);
+            return $this->SendDataToParent(json_encode([
+                'DataID' => "{95E9AD15-F5B0-01C6-4FF2-9093B9BD4E36}",
+                'Buffer' => utf8_encode(json_encode($sendData))
+            ]));
         }
 
-        public function Trigger(){
+        public function CreateMediaFile(){
+            $sendData = array("cmd" => "GetLink");
+            $rData = $this->SendDataToParent(json_encode([
+                'DataID' => "{95E9AD15-F5B0-01C6-4FF2-9093B9BD4E36}",
+                'Buffer' => utf8_encode(json_encode($sendData))
+            ]));
+            if(empty($rData)) return;
 
-            $id = $this->InstanceID;
-            $pid = IPS_GetParent($id);
-            $camera = $this->ReadPropertyString("ShortName");
+            $rData = json_decode($rData, true);
 
-            $sid = BlueIris_Login($pid);
-            if($sid != "ERROR")
-                return BlueIris_Trigger($pid, $sid, $camera);
-            else
-                return  "ERROR";
+            $MediaID = @IPS_GetMediaIDByName("Stream", $this->InstanceID);
+            if($MediaID === False){
+                if(!empty($rData["user"]) && !empty($rData["pw"]))
+                    $ImageFile = $rData["link"]."/mjpg/". $this->ReadPropertyString("ShortName"). "/video.mjpg?user=".$rData["user"]."&pw=".$rData["pw"]; // Image-Datei
+                else
+                    $ImageFile = $rData["link"]."/mjpg/". $this->ReadPropertyString("ShortName"). "/video.mjpg";
+
+                $MediaID = IPS_CreateMedia(3);                  // Image im MedienPool anlegen
+                IPS_SetMediaFile($MediaID, $ImageFile, true);   // Image im MedienPool mit Image-Datei verbinden
+                IPS_SetName($MediaID, "Stream"); // Medienobjekt benennen
+                IPS_SetParent($MediaID, $this->InstanceID);
+            }
         }
     }
-?>
